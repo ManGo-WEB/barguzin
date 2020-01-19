ff19a683b671b20f5cd2b3820317364a368823cd
ManGo-WEB <info@mango-web.ru>
2020-01-19 16:45:41 +0800

    Add libs.js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,1 @@
-//@@include('../../node_modules/jquery/dist/jquery.js');
-//@@include('../../node_modules/bootstrap/dist/js/bootstrap.js');
-
-//='../../node_modules/jquery/dist/jquery.js'
-//='../../node_modules/bootstrap/dist/js/bootstrap.js'
+//='libs.js'
