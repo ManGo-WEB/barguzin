3b48d50fccedf5ab865738eba874ebd0c962749f
ManGo-WEB <info@mango-web.ru>
2020-01-19 16:39:49 +0800

    Replacement gulp-file-include to gulp-rigger

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+//@@include('../../node_modules/jquery/dist/jquery.js');
+//@@include('../../node_modules/bootstrap/dist/js/bootstrap.js');
+
+//='../../node_modules/jquery/dist/jquery.js'
+//='../../node_modules/bootstrap/dist/js/bootstrap.js'
